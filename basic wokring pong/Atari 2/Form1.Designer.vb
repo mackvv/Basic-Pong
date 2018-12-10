6e446829d69e5533d83c5b98a722bbdbbdc980ce
mackvv <mackenzievanvliet@gmail.com>
2018-12-10 08:27:11 -0500

    Add files via upload

---
+++ basic wokring pong/Atari 2/Form1.Designer.vb
@@ -0,0 +1,391 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.borderBottom = New System.Windows.Forms.PictureBox()
+        Me.borderTop = New System.Windows.Forms.PictureBox()
+        Me.borderLeft = New System.Windows.Forms.PictureBox()
+        Me.borderRight = New System.Windows.Forms.PictureBox()
+        Me.brick1 = New System.Windows.Forms.PictureBox()
+        Me.brick2 = New System.Windows.Forms.PictureBox()
+        Me.brick3 = New System.Windows.Forms.PictureBox()
+        Me.brick4 = New System.Windows.Forms.PictureBox()
+        Me.brick5 = New System.Windows.Forms.PictureBox()
+        Me.brick10 = New System.Windows.Forms.PictureBox()
+        Me.brick9 = New System.Windows.Forms.PictureBox()
+        Me.brick8 = New System.Windows.Forms.PictureBox()
+        Me.brick7 = New System.Windows.Forms.PictureBox()
+        Me.brick6 = New System.Windows.Forms.PictureBox()
+        Me.brick15 = New System.Windows.Forms.PictureBox()
+        Me.brick14 = New System.Windows.Forms.PictureBox()
+        Me.brick13 = New System.Windows.Forms.PictureBox()
+        Me.brick12 = New System.Windows.Forms.PictureBox()
+        Me.brick11 = New System.Windows.Forms.PictureBox()
+        Me.brick20 = New System.Windows.Forms.PictureBox()
+        Me.brick19 = New System.Windows.Forms.PictureBox()
+        Me.brick18 = New System.Windows.Forms.PictureBox()
+        Me.brick17 = New System.Windows.Forms.PictureBox()
+        Me.brick16 = New System.Windows.Forms.PictureBox()
+        Me.tmrMoveBall = New System.Windows.Forms.Timer(Me.components)
+        Me.picBall = New System.Windows.Forms.PictureBox()
+        Me.picBar = New System.Windows.Forms.PictureBox()
+        CType(Me.borderBottom, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.borderTop, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.borderLeft, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.borderRight, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.brick1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.brick2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.brick3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.brick4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.brick5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.brick10, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.brick9, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.brick8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.brick7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.brick6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.brick15, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.brick14, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.brick13, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.brick12, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.brick11, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.brick20, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.brick19, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.brick18, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.brick17, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.brick16, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picBall, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picBar, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'borderBottom
+        '
+        Me.borderBottom.Location = New System.Drawing.Point(-4, 321)
+        Me.borderBottom.Name = "borderBottom"
+        Me.borderBottom.Size = New System.Drawing.Size(395, 51)
+        Me.borderBottom.TabIndex = 12
+        Me.borderBottom.TabStop = False
+        '
+        'borderTop
+        '
+        Me.borderTop.Location = New System.Drawing.Point(-4, -34)
+        Me.borderTop.Name = "borderTop"
+        Me.borderTop.Size = New System.Drawing.Size(395, 39)
+        Me.borderTop.TabIndex = 11
+        Me.borderTop.TabStop = False
+        '
+        'borderLeft
+        '
+        Me.borderLeft.Location = New System.Drawing.Point(-41, -4)
+        Me.borderLeft.Name = "borderLeft"
+        Me.borderLeft.Size = New System.Drawing.Size(44, 366)
+        Me.borderLeft.TabIndex = 10
+        Me.borderLeft.TabStop = False
+        '
+        'borderRight
+        '
+        Me.borderRight.Location = New System.Drawing.Point(380, -4)
+        Me.borderRight.Name = "borderRight"
+        Me.borderRight.Size = New System.Drawing.Size(44, 366)
+        Me.borderRight.TabIndex = 9
+        Me.borderRight.TabStop = False
+        '
+        'brick1
+        '
+        Me.brick1.Location = New System.Drawing.Point(9, 13)
+        Me.brick1.Name = "brick1"
+        Me.brick1.Size = New System.Drawing.Size(67, 27)
+        Me.brick1.TabIndex = 13
+        Me.brick1.TabStop = False
+        Me.brick1.Tag = "11"
+        '
+        'brick2
+        '
+        Me.brick2.Location = New System.Drawing.Point(83, 13)
+        Me.brick2.Name = "brick2"
+        Me.brick2.Size = New System.Drawing.Size(67, 27)
+        Me.brick2.TabIndex = 14
+        Me.brick2.TabStop = False
+        Me.brick2.Tag = "12"
+        '
+        'brick3
+        '
+        Me.brick3.Location = New System.Drawing.Point(157, 13)
+        Me.brick3.Name = "brick3"
+        Me.brick3.Size = New System.Drawing.Size(67, 27)
+        Me.brick3.TabIndex = 15
+        Me.brick3.TabStop = False
+        '
+        'brick4
+        '
+        Me.brick4.Location = New System.Drawing.Point(232, 13)
+        Me.brick4.Name = "brick4"
+        Me.brick4.Size = New System.Drawing.Size(67, 27)
+        Me.brick4.TabIndex = 16
+        Me.brick4.TabStop = False
+        '
+        'brick5
+        '
+        Me.brick5.Location = New System.Drawing.Point(306, 13)
+        Me.brick5.Name = "brick5"
+        Me.brick5.Size = New System.Drawing.Size(67, 27)
+        Me.brick5.TabIndex = 17
+        Me.brick5.TabStop = False
+        '
+        'brick10
+        '
+        Me.brick10.Location = New System.Drawing.Point(306, 46)
+        Me.brick10.Name = "brick10"
+        Me.brick10.Size = New System.Drawing.Size(67, 27)
+        Me.brick10.TabIndex = 22
+        Me.brick10.TabStop = False
+        '
+        'brick9
+        '
+        Me.brick9.Location = New System.Drawing.Point(232, 46)
+        Me.brick9.Name = "brick9"
+        Me.brick9.Size = New System.Drawing.Size(67, 27)
+        Me.brick9.TabIndex = 21
+        Me.brick9.TabStop = False
+        '
+        'brick8
+        '
+        Me.brick8.Location = New System.Drawing.Point(157, 46)
+        Me.brick8.Name = "brick8"
+        Me.brick8.Size = New System.Drawing.Size(67, 27)
+        Me.brick8.TabIndex = 20
+        Me.brick8.TabStop = False
+        '
+        'brick7
+        '
+        Me.brick7.Location = New System.Drawing.Point(83, 46)
+        Me.brick7.Name = "brick7"
+        Me.brick7.Size = New System.Drawing.Size(67, 27)
+        Me.brick7.TabIndex = 19
+        Me.brick7.TabStop = False
+        '
+        'brick6
+        '
+        Me.brick6.Location = New System.Drawing.Point(9, 46)
+        Me.brick6.Name = "brick6"
+        Me.brick6.Size = New System.Drawing.Size(67, 27)
+        Me.brick6.TabIndex = 18
+        Me.brick6.TabStop = False
+        '
+        'brick15
+        '
+        Me.brick15.Location = New System.Drawing.Point(306, 79)
+        Me.brick15.Name = "brick15"
+        Me.brick15.Size = New System.Drawing.Size(67, 27)
+        Me.brick15.TabIndex = 27
+        Me.brick15.TabStop = False
+        '
+        'brick14
+        '
+        Me.brick14.Location = New System.Drawing.Point(232, 79)
+        Me.brick14.Name = "brick14"
+        Me.brick14.Size = New System.Drawing.Size(67, 27)
+        Me.brick14.TabIndex = 26
+        Me.brick14.TabStop = False
+        '
+        'brick13
+        '
+        Me.brick13.Location = New System.Drawing.Point(157, 79)
+        Me.brick13.Name = "brick13"
+        Me.brick13.Size = New System.Drawing.Size(67, 27)
+        Me.brick13.TabIndex = 25
+        Me.brick13.TabStop = False
+        '
+        'brick12
+        '
+        Me.brick12.Location = New System.Drawing.Point(83, 79)
+        Me.brick12.Name = "brick12"
+        Me.brick12.Size = New System.Drawing.Size(67, 27)
+        Me.brick12.TabIndex = 24
+        Me.brick12.TabStop = False
+        '
+        'brick11
+        '
+        Me.brick11.Location = New System.Drawing.Point(9, 79)
+        Me.brick11.Name = "brick11"
+        Me.brick11.Size = New System.Drawing.Size(67, 27)
+        Me.brick11.TabIndex = 23
+        Me.brick11.TabStop = False
+        '
+        'brick20
+        '
+        Me.brick20.Location = New System.Drawing.Point(306, 112)
+        Me.brick20.Name = "brick20"
+        Me.brick20.Size = New System.Drawing.Size(67, 27)
+        Me.brick20.TabIndex = 32
+        Me.brick20.TabStop = False
+        '
+        'brick19
+        '
+        Me.brick19.Location = New System.Drawing.Point(232, 112)
+        Me.brick19.Name = "brick19"
+        Me.brick19.Size = New System.Drawing.Size(67, 27)
+        Me.brick19.TabIndex = 31
+        Me.brick19.TabStop = False
+        '
+        'brick18
+        '
+        Me.brick18.Location = New System.Drawing.Point(157, 112)
+        Me.brick18.Name = "brick18"
+        Me.brick18.Size = New System.Drawing.Size(67, 27)
+        Me.brick18.TabIndex = 30
+        Me.brick18.TabStop = False
+        '
+        'brick17
+        '
+        Me.brick17.Location = New System.Drawing.Point(83, 112)
+        Me.brick17.Name = "brick17"
+        Me.brick17.Size = New System.Drawing.Size(67, 27)
+        Me.brick17.TabIndex = 29
+        Me.brick17.TabStop = False
+        '
+        'brick16
+        '
+        Me.brick16.Location = New System.Drawing.Point(9, 112)
+        Me.brick16.Name = "brick16"
+        Me.brick16.Size = New System.Drawing.Size(67, 27)
+        Me.brick16.TabIndex = 28
+        Me.brick16.TabStop = False
+        '
+        'tmrMoveBall
+        '
+        Me.tmrMoveBall.Enabled = True
+        Me.tmrMoveBall.Interval = 10
+        '
+        'picBall
+        '
+        Me.picBall.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.picBall.Location = New System.Drawing.Point(24, 181)
+        Me.picBall.Name = "picBall"
+        Me.picBall.Size = New System.Drawing.Size(12, 11)
+        Me.picBall.TabIndex = 33
+        Me.picBall.TabStop = False
+        '
+        'picBar
+        '
+        Me.picBar.BackColor = System.Drawing.Color.White
+        Me.picBar.Location = New System.Drawing.Point(109, 304)
+        Me.picBar.Name = "picBar"
+        Me.picBar.Size = New System.Drawing.Size(135, 11)
+        Me.picBar.TabIndex = 54
+        Me.picBar.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(384, 362)
+        Me.Controls.Add(Me.picBar)
+        Me.Controls.Add(Me.picBall)
+        Me.Controls.Add(Me.brick20)
+        Me.Controls.Add(Me.brick19)
+        Me.Controls.Add(Me.brick18)
+        Me.Controls.Add(Me.brick17)
+        Me.Controls.Add(Me.brick16)
+        Me.Controls.Add(Me.brick15)
+        Me.Controls.Add(Me.brick14)
+        Me.Controls.Add(Me.brick13)
+        Me.Controls.Add(Me.brick12)
+        Me.Controls.Add(Me.brick11)
+        Me.Controls.Add(Me.brick10)
+        Me.Controls.Add(Me.brick9)
+        Me.Controls.Add(Me.brick8)
+        Me.Controls.Add(Me.brick7)
+        Me.Controls.Add(Me.brick6)
+        Me.Controls.Add(Me.brick5)
+        Me.Controls.Add(Me.brick4)
+        Me.Controls.Add(Me.brick3)
+        Me.Controls.Add(Me.brick2)
+        Me.Controls.Add(Me.brick1)
+        Me.Controls.Add(Me.borderBottom)
+        Me.Controls.Add(Me.borderTop)
+        Me.Controls.Add(Me.borderLeft)
+        Me.Controls.Add(Me.borderRight)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(400, 400)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.borderBottom, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.borderTop, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.borderLeft, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.borderRight, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.brick1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.brick2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.brick3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.brick4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.brick5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.brick10, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.brick9, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.brick8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.brick7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.brick6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.brick15, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.brick14, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.brick13, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.brick12, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.brick11, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.brick20, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.brick19, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.brick18, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.brick17, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.brick16, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picBall, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picBar, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents borderBottom As System.Windows.Forms.PictureBox
+    Friend WithEvents borderTop As System.Windows.Forms.PictureBox
+    Friend WithEvents borderLeft As System.Windows.Forms.PictureBox
+    Friend WithEvents borderRight As System.Windows.Forms.PictureBox
+    Friend WithEvents brick1 As System.Windows.Forms.PictureBox
+    Friend WithEvents brick2 As System.Windows.Forms.PictureBox
+    Friend WithEvents brick3 As System.Windows.Forms.PictureBox
+    Friend WithEvents brick4 As System.Windows.Forms.PictureBox
+    Friend WithEvents brick5 As System.Windows.Forms.PictureBox
+    Friend WithEvents brick10 As System.Windows.Forms.PictureBox
+    Friend WithEvents brick9 As System.Windows.Forms.PictureBox
+    Friend WithEvents brick8 As System.Windows.Forms.PictureBox
+    Friend WithEvents brick7 As System.Windows.Forms.PictureBox
+    Friend WithEvents brick6 As System.Windows.Forms.PictureBox
+    Friend WithEvents brick15 As System.Windows.Forms.PictureBox
+    Friend WithEvents brick14 As System.Windows.Forms.PictureBox
+    Friend WithEvents brick13 As System.Windows.Forms.PictureBox
+    Friend WithEvents brick12 As System.Windows.Forms.PictureBox
+    Friend WithEvents brick11 As System.Windows.Forms.PictureBox
+    Friend WithEvents brick20 As System.Windows.Forms.PictureBox
+    Friend WithEvents brick19 As System.Windows.Forms.PictureBox
+    Friend WithEvents brick18 As System.Windows.Forms.PictureBox
+    Friend WithEvents brick17 As System.Windows.Forms.PictureBox
+    Friend WithEvents brick16 As System.Windows.Forms.PictureBox
+    Friend WithEvents tmrMoveBall As System.Windows.Forms.Timer
+    Friend WithEvents picBall As System.Windows.Forms.PictureBox
+    Friend WithEvents picBar As System.Windows.Forms.PictureBox
+
+End Class
---
+++ basic wokring pong/Atari 2/Form1.vb
@@ -0,0 +1,178 @@
+﻿Imports System.Math
+Public Class Form1
+
+    Private Sub Form1_KeyDown(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles MyBase.KeyDown
+        Select Case e.KeyCode
+            Case Keys.Left
+                If Me.picBar.Bounds.IntersectsWith(Me.borderLeft.Bounds) Then
+                Else
+                    Me.picBar.Left -= 5
+
+                End If
+
+            Case Keys.Right
+                If Me.picBar.Bounds.IntersectsWith(Me.borderRight.Bounds) Then
+                Else
+                    Me.picBar.Left += 5
+
+                End If
+        End Select
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Call createBricks()
+    End Sub
+
+    Function RndInt(ByVal HighNum As Integer, ByVal LowNum As Integer) As Integer
+        'returns random number
+        Dim randomNum As Integer
+
+        Randomize()
+        randomNum = Int((HighNum - LowNum + 1) * Rnd()) + LowNum
+
+        Return randomNum
+    End Function
+
+    Private Sub tmrMoveBall_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmrMoveBall.Tick
+        Static UDdirection As String
+        Static LRdirection As String
+        Static EndGame As Integer
+
+
+        Static barBally As Double
+        Static barBallx As Double
+        Static boxBally As Double
+        Static boxBallx As Double
+        Static distanceFromBartoBox As Double
+        Dim height As Double
+
+
+        If Me.picBall.Bounds.IntersectsWith(Me.borderLeft.Bounds) Then
+            LRdirection = "right"
+        End If
+
+        If Me.picBall.Bounds.IntersectsWith(Me.borderRight.Bounds) Then
+            LRdirection = "left"
+        End If
+
+        If Me.picBall.Bounds.IntersectsWith(Me.borderTop.Bounds) Then
+            UDdirection = "down"
+        End If
+
+        If Me.picBall.Bounds.IntersectsWith(Me.borderBottom.Bounds) Then
+            picBall.Location = New Point(24, 171)
+        End If
+
+        For i = 1 To 20
+            If Me.Controls("brick" & i).Visible = True Then
+                If Me.picBall.Bounds.IntersectsWith(Me.Controls("brick" & i).Bounds) Then
+                    Beep()
+                    UDdirection = "down"
+                    Me.Controls("brick" & i).Visible = False
+                    EndGame += 1
+
+                    boxBallx = picBall.Location.X
+                    boxBally = picBall.Location.Y
+                    Dim d As Double
+
+                    d = (((boxBallx + barBallx) ^ 2) - ((boxBally + barBally) ^ 2))
+                    distanceFromBartoBox = Sqrt(d)
+
+
+                End If
+            End If
+        Next i
+
+        Dim xbar As Double
+        Dim xball As Double
+
+        If picBall.Bounds.IntersectsWith(picBar.Bounds) Then
+            UDdirection = "up"
+            xbar = picBar.Location.X
+            xball = picBall.Location.X
+            Select Case xball
+                Case xbar To xbar + 45
+                    LRdirection = "left"
+                Case xbar + 90 To xbar + 135
+                    LRdirection = "right"
+            End Select
+            xbar = Nothing
+            xball = Nothing
+
+            barBally = picBall.Location.Y
+            barBallx = picBall.Location.X
+        End If
+
+        If UDdirection = "up" Then
+            picBall.Top -= 1
+        Else
+            picBall.Top += 1
+        End If
+
+        If LRdirection = "left" Then
+            picBall.Left -= 1
+        Else
+            picBall.Left += 1
+        End If
+
+        If EndGame = 20 Then
+            tmrMoveBall.Enabled = False
+            MessageBox.Show("you won")
+            Call createBricks()
+            picBall.Location = New Point(24, 171)
+            UDdirection = "down"
+            LRdirection = "left"
+            EndGame = 0
+            tmrMoveBall.Enabled = True
+        End If
+
+    End Sub
+
+    Sub createBricks()
+        Dim color As Integer
+        Dim i As Integer
+
+        For i = 1 To 20
+            Me.Controls("brick" & i).Visible = True
+        Next
+
+        color = RndInt(3, 1)
+        For i = 1 To 20
+            If color = 1 Then
+                Me.Controls("brick" & i).BackColor = Drawing.Color.LemonChiffon
+            ElseIf color = 2 Then
+                Me.Controls("brick" & i).BackColor = Drawing.Color.Purple
+            Else
+                Me.Controls("brick" & i).BackColor = Drawing.Color.Pink
+            End If
+        Next i
+
+        color = RndInt(3, 1)
+        For i = 1 To 20 Step 2
+            If color = 1 Then
+                Me.Controls("brick" & i).BackColor = Drawing.Color.LimeGreen
+            ElseIf color = 2 Then
+                Me.Controls("brick" & i).BackColor = Drawing.Color.Orange
+            Else
+                Me.Controls("brick" & i).BackColor = Drawing.Color.SpringGreen
+            End If
+        Next i
+
+        color = RndInt(3, 1)
+        For i = 1 To 20 Step 3
+            If color = 1 Then
+                Me.Controls("brick" & i).BackColor = Drawing.Color.Yellow
+            ElseIf color = 2 Then
+                Me.Controls("brick" & i).BackColor = Drawing.Color.Lavender
+            Else
+                Me.Controls("brick" & i).BackColor = Drawing.Color.Red
+            End If
+        Next i
+
+        'top row 
+
+
+
+    End Sub
+
+End Class
